41b59d0382deba9920c55411fd4b0c02d526acbf
rknoche6 <rknoche@purdue.edu>
2022-03-01 16:31:43 +0100

    migrated from react testing library to enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-test('renders without crashing', () => {
-  const div = document.createElement('div');
-  render(<App />, div);
+test("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
 });
