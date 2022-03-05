40fd2f60c7a2a610d05143bc5af6d8becd7f11a7
rknoche6 <rknoche@purdue.edu>
2022-03-05 11:31:10 +0100

    added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+
+import "@testing-library/jest-dom";
+import { getByTestId } from "@testing-library/dom";
+import { render } from "@testing-library/react";
+
 import App from "./App";
 
 test("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
 });
+
+
+it("mock testing using enzyme", async (done) => {
+  const mockResponse = {
+    name: "Paris",
+    main: {
+      temp: "210",
+    },
+    weather: [
+      {
+        id: 700,
+        main: "Cloudy",
+      },
+    ],
+  };
+  const mockJsonPromise = Promise.resolve(mockResponse);
+  const mockFetchPromise = Promise.resolve({
+    json: () => mockJsonPromise,
+  });
+  jest.spyOn(global, "fetch").mockImplementation(() => mockFetchPromise);
+  const wrapper = shallow(<App />);
+
+  expect(global.fetch).toHaveBeenCalledTimes(1);
+  process.nextTick(() => {
+    expect(wrapper).toMatchSnapshot();
+
+    global.fetch.mockClear();
+    done();
+  });
+});
+
